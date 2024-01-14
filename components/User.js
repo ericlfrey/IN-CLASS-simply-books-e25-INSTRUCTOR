06a3743659ebb350164243d9a7c7106c5997c956
Eric Frey <ericlfrey@gmail.com>
2024-01-14 10:29:59 -0600

    Verified Read Books, Profile page.

--- components/User.js
+++ components/User.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 export default function User({ user }) {
   return (
     <>
-      <img src={user.photoURL} alt="" />
+      <img src={user.photoURL} alt="User Profile" style={{ width: '150px', borderRadius: '50%' }} />
       <h1>Name: {user.displayName}</h1>
       <h1>Email: {user.email}</h1>
       <h1>Last Login {user.metadata.lastSignInTime}</h1>
