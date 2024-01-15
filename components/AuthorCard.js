cc8230fa6e06018ceb6b6cef7d6fc69106e76e27
Eric Frey <ericlfrey@gmail.com>
2024-01-15 11:40:09 -0600

    View all authors and view single author is working correctly/

--- components/AuthorCard.js
+++ components/AuthorCard.js
@@ -1,12 +1,45 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { deleteAuthorBooks } from '../api/mergedData';
 
-function AuthorCard() {
+function AuthorCard({ authorObj, onUpdate }) {
+  const deleteThisAuthor = () => {
+    if (window.confirm(`Delete ${authorObj.first_name} ${authorObj.last_name}?`)) {
+      deleteAuthorBooks(authorObj.firebaseKey).then(() => onUpdate());
+    }
+  };
   return (
-    <div>AuthorCard</div>
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Body>
+        <Card.Title>{authorObj.first_name} {authorObj.last_name}</Card.Title>
+        <p className="card-text bold">{authorObj.favorite && <span>❤️ Favorite<br /></span>}</p>
+        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
+        <Link href={`/author/${authorObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/author/edit/${authorObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisAuthor} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
 
-AuthorCard.propTypes = {};
+AuthorCard.propTypes = {
+  authorObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    email: PropTypes.string,
+    favorite: PropTypes.bool,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
 
 export default AuthorCard;
--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,7 +1,30 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { viewAuthorDetails } from '../../api/mergedData';
+import BookCard from '../../components/BookCard';
 
 export default function ViewAuthorPage() {
+  const [author, setAuthor] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    viewAuthorDetails(firebaseKey).then(setAuthor);
+  }, []);
   return (
-    <h1>ViewAuthorPage</h1>
+    <>
+      <div className="text-white ms-5 mt-5 details">
+        <h2>{author.first_name} {author.last_name} {author.favorite && '❤️'}</h2>
+        <a href={`mailto:${author.email}`}>{author.email}</a>
+      </div>
+      <hr className="text-white" />
+      <div className="d-flex flex-wrap">
+        {author.books?.length > 0
+          ? author.books?.map((book) => (
+            <BookCard key={book.firebaseKey} bookObj={book} />
+          ))
+          : <h2>No Books</h2>}
+      </div>
+    </>
   );
 }
